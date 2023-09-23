f42f07d6f675099b5ba8cb3d2f7edb28733ce94e
Arshdeep_011 <ashok2468rajpute@gmail.com>
2023-09-23 18:40:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import axios from "axios";
+
+function App() {
+  //function handleLogin = async ()=>
+  
+  async function handleLogin(){
+    
+
+try {
+  console.log("ye");
+  var t=await axios.post('https://testb.onrender.com/login',{useremail:"xyz1@gmail.com",password:'99999999'},
+  {
+   headers: {
+    'Content-Type': 'application/x-www-form-urlencoded' 
+     } ,
+     withCredentials: true
+       
+  });
+
+  console.log("=",t);
+} catch (err) {
+ console.log(err); 
+}
+
+  
+  }
+  return (
+   <>
+   <button  onClick={handleLogin}>Login</button>
+   </>
+   
+  );
+}
+
+export default App;
